e21702bdabe9924fe52869ec7e1cfa16177e0e96
DESKTOP-LCD\lcd26 <dan.luongcong@gameloft.com>
2024-05-31 10:26:13 +0700

    + simple editor

--- Assets/Sandbox/Editor/Editors/SandboxSettingsEditor.cs
+++ Assets/Sandbox/Editor/Editors/SandboxSettingsEditor.cs
@@ -1,8 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Sandbox.Runtime;
 using UnityEditor;
-using UnityEngine;
+using UnityEditor.UIElements;
 using UnityEngine.UIElements;
 
 namespace Sandbox.Editor.Editors
@@ -10,13 +11,55 @@ namespace Sandbox.Editor.Editors
     [CustomEditor(typeof(SandboxSettings), true)]
     public class SandboxSettingsEditor : UnityEditor.Editor
     {
+        private VisualElement root { get; set; }
         public override VisualElement CreateInspectorGUI()
         {
-            var root = new VisualElement();
-
-
+            FindSerializedProperties();
+            InitializeEditor();
+            Compose();
 
             return root;
         }
+
+        #region Serialized Properties
+        private SerializedProperty propertyTestString { get; set; }
+        private SerializedProperty propertyTestInt { get; set; }
+        private SerializedProperty propertyTestFloat { get; set; }
+
+
+        private void FindSerializedProperties()
+        {
+            propertyTestString = serializedObject.FindProperty(nameof(SandboxSettings.TestString));
+            propertyTestInt = serializedObject.FindProperty(nameof(SandboxSettings.TestInt));
+            propertyTestFloat = serializedObject.FindProperty(nameof(SandboxSettings.TestFloat));
+        }
+        #endregion
+
+        #region Initialize Editor
+
+        private PropertyField fieldTestString { get; set; }
+        private PropertyField fieldTestInt { get; set; }
+        private PropertyField fieldTestFloat { get; set; }
+
+        private void InitializeEditor()
+        {
+            root = new VisualElement();
+
+            fieldTestString = new PropertyField(propertyTestString);
+            fieldTestInt = new PropertyField(propertyTestInt);
+            fieldTestFloat = new PropertyField(propertyTestFloat);
+        }
+        #endregion
+
+        #region Compose
+        private void Compose()
+        {
+            root.Add(new Label("Sandbox Settings"));
+            root.Add(fieldTestString);
+            root.Add(fieldTestInt);
+            root.Add(fieldTestFloat);
+        }
+        #endregion
+
     }
 }
